ab0b977c723d03a5599e724178ca23e285ccc478
heyhlxy <heyhlxy@163.com>
2019-06-14 20:37:01 +0800

    practice08 completed

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,29 @@
 package practice08;
 
 public class Klass {
+    
+	private int number;
+	private Student leader;
+	
+	public int getNumber(){
+		return number;
+	}
+	
+	public Student getLeader(){
+		return leader;
+	}
+	
+	public String getDisplayName(){
+		return "Class "+ number;
+	}
+
+	public Klass(int number) {
+		super();
+		this.number = number;
+	}
+	
+	public void assignLeader(Student student){
+		 this.leader = student;
+		 leader.setLeaderStatus(true);
+	}	
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,29 @@
 package practice08;
 
-public class Student {
+public class Student extends Person{
+    
+    private Klass klass;
+	private boolean isLeader = false;
+	
+	public Student(int id, String name, int age, Klass klass) {
+		super(id, name, age);
+		this.klass = klass;
+	}
+    
+	public Klass getKlass(){
+		return klass;
+	}
+	
+	public void setLeaderStatus(boolean isLeader){
+		this.isLeader = isLeader;
+	}
+	
+	@Override
+	public String introduce(){
+		if(isLeader){
+			return super.introduce()+" I am a Student. I am Leader of " + klass.getDisplayName() + ".";
+		} else{
+			return super.introduce()+" I am a Student. I am at " + klass.getDisplayName() +".";
+		}
+	}
 }
