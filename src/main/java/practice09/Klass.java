d13d8d9c0e1c2bd7f5edff4e3cb25803d0dd4adc
heyhlxy <heyhlxy@163.com>
2019-06-14 20:40:47 +0800

    practice09 completed

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,37 @@
 package practice09;
 
 public class Klass {
+	
+	private int number;
+	private Student leader;
+	
+	public int getNumber(){
+		return number;
+	}
+	
+	public Student getLeader(){
+		return leader;
+	}
+	
+	public String getDisplayName(){
+		return "Class "+ number;
+	}
+
+	public Klass(int number) {
+		super();
+		this.number = number;
+	}
+	
+	public void assignLeader(Student student){
+		if(student.getKlass().getNumber()!=number){
+			System.out.print("It is not one of us.\n");
+			return;
+		} 
+	    this.leader = student;
+		leader.setLeaderStatus(true);
+	}	
+	
+	public void appendMember(Student student){
+		student.setKlass(this);
+	}
 }
