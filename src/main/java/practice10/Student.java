8960d89154702f7aa2a0d42aa1dabcd122153f46
heyhlxy <heyhlxy@163.com>
2019-06-14 20:41:12 +0800

    practice10 completed

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,33 @@
 package practice10;
 
-public class Student {
+public class Student extends Person{
+	
+	private Klass klass;
+	private boolean isLeader = false;
+	
+	public Student(int id, String name, int age, Klass klass) {
+		super(id, name, age);
+		this.klass = klass;
+	}
+    
+	public Klass getKlass(){
+		return klass;
+	}
+	
+	public void setKlass(Klass klass){
+		this.klass = klass;
+	}
+	
+	public void setLeaderStatus(boolean isLeader){
+		this.isLeader = isLeader;
+	}
+	
+	@Override
+	public String introduce(){
+		if(isLeader){
+			return super.introduce()+" I am a Student. I am Leader of " + klass.getDisplayName() + ".";
+		} else{
+			return super.introduce()+" I am a Student. I am at " + klass.getDisplayName() +".";
+		}
+	}
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,49 @@
 package practice10;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person{
+    
+	private LinkedList<Klass> klasses;
+	
+	public Teacher(int id, String name, int age, LinkedList<Klass> klasses) {
+		super(id, name, age);
+		this.klasses = klasses;
+	}
+    
+	public Teacher(int id, String name, int age) {
+		super(id, name, age);
+	}
+	
+	public LinkedList<Klass> getClasses(){
+		return klasses;
+	}
+	
+	@Override
+	public String introduce(){
+		StringBuilder klassesNum = new StringBuilder();
+		if(klasses != null){
+			if(klasses.size() != 0){
+				klassesNum.append(klasses.get(0).getNumber());
+				for (int i = 1; i < klasses.size(); i++) {
+					klassesNum.append(", "+ klasses.get(i).getNumber());
+				}
+				return super.introduce()+" I am a Teacher. I teach Class " + klassesNum +".";
+			}
+			return super.introduce()+" I am a Teacher. I teach No Class.";
+		} else {
+            return super.introduce()+" I am a Teacher. I teach No Class.";			
+		}	
+	}
+	
+	public String introduceWith(Student student){
+			if(isTeaching(student)){
+				return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+			} 
+		return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+	}
+	
+	public boolean isTeaching(Student student){
+		return klasses.contains(student.getKlass());
+	}
 }
