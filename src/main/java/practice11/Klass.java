e1267f6dafe41d4a636998142972399a705370e1
heyhlxy <heyhlxy@163.com>
2019-06-14 20:41:43 +0800

    practice11 completed

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,57 @@
 package practice11;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+	
+	private int number;
+	private Student leader;
+	private List<Teacher> teacherList = new ArrayList<Teacher>(); //Observers
+	
+	public Klass(int number) {
+		super();
+		this.number = number;
+	}
+	
+	public int getNumber(){
+		return number;
+	}
+	
+	public Student getLeader(){
+		return leader;
+	}
+	
+	public String getDisplayName(){
+		return "Class "+ number;
+	}
+
+	public void assignLeader(Student student){
+		if(student.getKlass().getNumber()!=number){
+			System.out.print("It is not one of us.\n");
+			return;
+		} 
+	    this.leader = student;
+		leader.setLeaderStatus(true);
+		notifyAllTeachers(student, true);
+	}	
+	
+	public void appendMember(Student student){
+		student.setKlass(this);
+		notifyAllTeachers(student, false);
+	}
+	
+	public boolean isIn(Student student){
+		return this.number == student.getKlass().getNumber();
+	}
+	
+	public void attachTeacher(Teacher teacher){
+		teacherList.add(teacher);
+	}
+	
+	public void notifyAllTeachers(Student student, boolean isLeader){
+		for(Teacher t : teacherList){
+			t.updateStudent(student, isLeader);
+		}
+	}
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,62 @@
 package practice11;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person{
+    
+	private LinkedList<Klass> klasses;
+	
+	public Teacher(int id, String name, int age, LinkedList<Klass> klasses) {
+		super(id, name, age);
+		this.klasses = klasses;
+		for(Klass klass : klasses){
+			klass.attachTeacher(this);
+		}
+	}
+    
+	public Teacher(int id, String name, int age) {
+		super(id, name, age);
+	}
+	
+	public LinkedList<Klass> getClasses(){
+		return klasses;
+	}
+	
+	@Override
+	public String introduce(){
+		StringBuilder klassesNum = new StringBuilder();
+		if(klasses != null){
+			if(klasses.size() != 0){
+				klassesNum.append(klasses.get(0).getNumber());
+				for (int i = 1; i < klasses.size(); i++) {
+					klassesNum.append(", "+ klasses.get(i).getNumber());
+				}
+				return super.introduce()+" I am a Teacher. I teach Class " + klassesNum +".";
+			}
+			return super.introduce()+" I am a Teacher. I teach No Class.";
+		} else {
+            return super.introduce()+" I am a Teacher. I teach No Class.";			
+		}	
+	}
+	
+	public String introduceWith(Student student){
+			if(isTeaching(student)){
+				return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+			} 
+		return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+	}
+	
+	public boolean isTeaching(Student student){
+		return klasses.contains(student.getKlass());
+	}
+	
+	public void updateStudent(Student student, boolean isLeader){
+		if(isLeader){
+			System.out.print("I am Tom. I know "+student.getName()+" become Leader of "
+					+ student.getKlass().getDisplayName() + ".\n");
+		} else {
+			System.out.print("I am Tom. I know "+student.getName()+" has joined "
+					+ student.getKlass().getDisplayName() + ".\n");
+		}
+	}
 }
